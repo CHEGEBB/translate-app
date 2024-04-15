4ffd2ccf575fff76ed27ad85e7ffd15906e86b57
CHEGEBB <chegephil24@gmail.com>
2024-04-15 17:19:38 +0300

    tsx

--- src/components/inputComponent.tsx
+++ src/components/inputComponent.tsx
@@ -5,6 +5,20 @@ const InputComponent = () => {
     return (
 
         <div className="output-card">
+<div className="side">
+Detect Language
+</div>
+<nav>
+  <div className="nav-item-english">
+    English
+  </div>
+  <div className="nav-item-french">
+    French
+  </div>
+  <div className="nav-item-spanish">
+    Spanish
+  </div>
+</nav>
         </div>
       );
 }
