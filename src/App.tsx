3ac5d6e233dd1bd54adff2059f2b5d083364a677
CHEGEBB <chegephil24@gmail.com>
2024-04-16 12:42:01 +0300

    scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import InputComponent from './components/inputComponent';
 import OutputComponent from './components/outputComponent';
+import Logo from './images/logo.svg';
 
 function App() {
   return (
     <div className="App">
+      <div className="center-logo">
+        <img src={Logo} alt="logo" />
+      </div>
       <div className="container">
       <div className="cards">
         <InputComponent />
