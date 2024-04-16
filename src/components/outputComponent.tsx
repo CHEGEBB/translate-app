5ab143bd649a72143cd8c52db24071ea3652e198
CHEGEBB <chegephil24@gmail.com>
2024-04-16 11:18:54 +0300

    react

--- src/components/outputComponent.tsx
+++ src/components/outputComponent.tsx
@@ -1,8 +1,33 @@
 import React from "react";
+import { useState } from "react";
 
-const OutputComponent = () => {
+const OutputComponent:React.FC = () => {
+  const [activeNavItem, setActiveNavItem] = useState<string>('English');
+  const handleNavItemClick = (navItem: string) => {
+    setActiveNavItem(navItem);
+  }
     return (
-        <div className="input-card">
+        <div className="output-card">
+           <nav>
+        <div
+          className={`nav-item ${activeNavItem === 'English' ? 'active' : ''}`}
+          onClick={() => handleNavItemClick('English')}
+        >
+          English
+        </div>
+        <div
+          className={`nav-item ${activeNavItem === 'French' ? 'active' : ''}`}
+          onClick={() => handleNavItemClick('French')}
+        >
+          French
+        </div>
+        <div
+          className={`nav-item ${activeNavItem === 'Spanish' ? 'active' : ''}`}
+          onClick={() => handleNavItemClick('Spanish')}
+        >
+          Spanish
+        </div>
+      </nav>
         </div>
       );
 }
