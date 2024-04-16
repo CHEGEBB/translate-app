d09fdee96adf5fe8a295e68c901fe90b47124c37
CHEGEBB <chegephil24@gmail.com>
2024-04-16 15:38:33 +0300

    ts

--- src/components/translationService.ts
+++ src/components/translationService.ts
@@ -1,16 +1,15 @@
 const translateText = async (text: string, fromLang: string, toLang: string): Promise<string> => {
-    try {
-      const response = await fetch(`https://api.mymemory.translated.net/get?q=${text}&langpair=${fromLang}|${toLang}`);
-      const data = await response.json();
-      if (data && data.responseData && data.responseData.translatedText) {
-        return data.responseData.translatedText;
-      } else {
-        throw new Error('Translation failed');
-      }
-    } catch (error) {
+  try {
+    const response = await fetch(`https://api.mymemory.translated.net/get?q=${text}&langpair=${fromLang}|${toLang}`);
+    const data = await response.json();
+    if (data && data.responseData && data.responseData.translatedText) {
+      return data.responseData.translatedText;
+    } else {
       throw new Error('Translation failed');
     }
+  } catch (error) {
+    throw new Error('Translation failed');
   }
-  
-  export default translateText;
-  
+};
+
+export default translateText;
